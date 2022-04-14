1ccdd397546e567720a349e4284ebde75b5c2506
andrehahn05 <andrehahn05@gmail.com>
2022-04-14 12:00:10 -0400

    components/Input

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TextInputProps} from 'react-native';
+import theme from '../../../global/styles/theme';
+import {Container} from './styles';
+
+const Input: React.FC<TextInputProps> = ({...otherProps}) => {
+  return (
+    <Container placeholderTextColor={theme.colors.gray500} {...otherProps} />
+  );
+};
+
+export default Input;
