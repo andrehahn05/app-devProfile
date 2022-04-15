191197baa1e4802d0a4ba8c86eca683d8b6f5fd6
andrehahn05 <andrehahn05@gmail.com>
2022-04-14 20:09:49 -0400

    btn-forgotPass

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -25,3 +25,13 @@ export const Logo = styled.Image`
   height: ${RFValue(160)}px;
   margin-bottom: ${RFValue(64)}px;
 `;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordTitle = styled.Text`
+  font-size: ${RFValue(16)}px;
+  font-weight: 600;
+  color: ${({theme}) => theme.colors.gray500};
+`;
